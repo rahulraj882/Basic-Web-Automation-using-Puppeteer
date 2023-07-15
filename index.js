8a2a27e2c2f40c3f67716d3de438bf88e8a038b8
Rahul Raj <rahul800902@gmail.com>
2023-07-16 00:49:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('https://swap.defillama.com/');
+    
     await page.waitForSelector('#react-select-2-input');
-
     // Fill the dropdown input field
     await page.focus('#react-select-2-input');
-    await page.keyboard.type('Arbitrum One');
+    await page.keyboard.type('Arbitrum');
   
     // Trigger an additional key press to select the option
     await page.keyboard.press('Enter');
   
-    const input = await page.$('#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-55ee4011-1.cZHlms > div.sc-55ee4011-3.dlZmAt > div.sc-55ee4011-0.iXoIVV > div.css-1urcov8 > div:nth-child(1) > div.css-1k491an > div > input');
+    const input = await page.$('#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-bb167634-1.bpASfZ > div.sc-bb167634-3.Lyrxc > div.sc-bb167634-0.bVZjlY > div.css-1urcov8 > div:nth-child(1) > div.css-1k491an > div > input');
     await input.click({ clickCount: 3 })
-    await page.type('#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-55ee4011-1.cZHlms > div.sc-55ee4011-3.dlZmAt > div.sc-55ee4011-0.iXoIVV > div.css-1urcov8 > div:nth-child(1) > div.css-1k491an > div > input','12');
+    await page.type('#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-bb167634-1.bpASfZ > div.sc-bb167634-3.Lyrxc > div.sc-bb167634-0.bVZjlY > div.css-1urcov8 > div:nth-child(1) > div.css-1k491an > div > input','12');
     await page.waitForSelector('button.chakra-button > span.chakra-text');
     // Click on the dropdown button
     await page.click('button.chakra-button > span.chakra-text');
@@ -40,17 +40,17 @@ const puppeteer = require('puppeteer');
     if (usdcOption) {
     await usdcOption.click();
     }
-    await page.waitForTimeout(500); // Wait for 500ms
-    await page.waitForSelector('#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-55ee4011-1.cZHlms > div.sc-55ee4011-3.dlZmAt > div.sc-55ee4011-2.fcGAPg > div:nth-child(5)');
+    //await page.waitForTimeout(500); // Wait for 500ms
+    //await page.waitForSelector('#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-55ee4011-1.cZHlms > div.sc-55ee4011-3.dlZmAt > div.sc-55ee4011-2.fcGAPg > div:nth-child(5)');
 
     // Find the route option using page.evaluate()
-    const routeOption = await page.evaluate(() => {
-        return document.querySelector('#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-55ee4011-1.cZHlms > div.sc-55ee4011-3.dlZmAt > div.sc-55ee4011-2.fcGAPg > div:nth-child(5)');
-    });
+    //const routeOption = await page.evaluate(() => {
+        //return document.querySelector('#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-55ee4011-1.cZHlms > div.sc-55ee4011-3.dlZmAt > div.sc-55ee4011-2.fcGAPg > div:nth-child(5)');
+    //});
     // Click on the route option
-    if (routeOption) {
-    await routeOption.click();
-    }
+    //if (routeOption) {
+    //await routeOption.click();
+    //}
  
    
 })();
